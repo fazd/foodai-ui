631f7dd46d2dcf47ba1091d6535555295c0581f0
Fabio <fabioz@uninorte.edu.co>
2020-11-08 18:23:55 -0500

    Add checkbox

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { withStyles } from '@material-ui/core/styles';
 import DefaultImage from "../../assets/defaultImage.jpg";
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Button from "@material-ui/core/Button";
+import Checkbox from '@material-ui/core/Checkbox';
+
 import "./Result.scss"
 
 
@@ -23,6 +25,106 @@ const BorderLinearProgress = withStyles((theme) => ({
 
 
 const Result = (props) => {
+  const [checked, setChecked] = useState({
+    One: false,
+    Two: false,
+    Three: false,
+    Four: false,
+    Five: false,
+  });
+
+  const handleChangeOne = (event) => {
+    //event.persist();
+    if(checked.One){
+      setChecked({...checked, One: false});
+    }
+    else{
+      setChecked({...checked, One: true});
+    }
+  };
+
+  const handleChangeTwo = (event) => {
+    event.persist();
+    if(checked.Two){
+      setChecked({...checked, Two: false});
+    }
+    else{
+      setChecked({...checked, Two: true});
+    }
+  };
+
+  const handleChangeThree = (event) => {
+    event.persist();
+    if(checked.Three){
+      setChecked({...checked, Three: false});
+    }
+    else{
+      setChecked({...checked, Three: true});
+    }
+  };
+
+  const handleChangeFour = (event) => {
+    event.persist();
+    if(checked.Four){
+      setChecked({...checked, Four: false});
+    }
+    else{
+      setChecked({...checked, Four: true});
+    }
+  };
+
+  const handleChangeFive = (event) => {
+    event.persist();
+    if(checked.Five){
+      setChecked({...checked, Five: false});
+    }
+    else{
+      setChecked({...checked, Five: true});
+    }
+  };
+
+  const handleEstimate = () => {
+    let categories = [];
+    if(checked.One){
+      categories.push(data[0].category);
+    }
+    if(checked.Two){
+      categories.push(data[1].category);
+    }
+    if(checked.Three){
+      categories.push(data[2].category);
+    }
+    if(checked.Four){
+      categories.push(data[3].category);
+    }
+    if(checked.Five){
+      categories.push(data[4].category);
+    }
+    console.log(categories);
+  }
+
+  const data = [
+    {
+      category: "carne",
+      value: "80"
+    },
+    {
+      category: "Pollo",
+      value: "70"
+    },
+    {
+      category: "Salmon",
+      value: "80"
+    },
+    {
+      category: "Puré de papa",
+      value: "60"
+    },
+    {
+      category: "Arroz",
+      value: "50"
+    },
+  ];
 
   return(
     <div className="result-main-container">
@@ -39,34 +141,93 @@ const Result = (props) => {
               Categorias
             </span>
           </div>
-          <div className="result-table">
+          <div className="result-table">            
             <div className="result-cat">
-              <span>Cat 1: 0%</span>
-              <BorderLinearProgress variant="determinate" value={80} />
+              <div className="bar">
+                <span>{data[0].category+': '+data[0].value}%</span>
+                <BorderLinearProgress variant="determinate" value={data[0].value} />
+              </div>
+              <div className="check">
+                <Checkbox
+                  checked={checked.One}
+                  onClick={handleChangeOne}
+                  name="one"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              </div>
             </div>
             <div className="result-cat">
-              <span>Cat 2: 0% </span>
-              <BorderLinearProgress variant="determinate" value={30} />
+              <div className="bar">
+                <span>{data[1].category+': '+data[1].value}%</span>
+                <BorderLinearProgress variant="determinate" value={data[1].value} />
+              </div>
+              <div className="check">
+                <Checkbox
+                  checked={checked.Two}
+                  name="two"
+                  onClick={handleChangeTwo}
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              </div>
             </div>
             <div className="result-cat">
-              <span>Cat 3: 0%</span>
-                <BorderLinearProgress variant="determinate" value={60} />
+              <div className="bar">
+                <span>{data[2].category+': '+data[2].value}%</span>
+                <BorderLinearProgress variant="determinate" value={data[2].value} />
+              </div>
+              <div className="check">
+                <Checkbox
+                  checked={checked.Three}
+                  onClick={handleChangeThree}
+                  name="three"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              </div>
             </div>
             <div className="result-cat">
-              <span>Cat 4: 0%</span>
-              <BorderLinearProgress variant="determinate" value={100} />
+              <div className="bar">
+                <span>{data[3].category+': '+data[3].value}%</span>
+                <BorderLinearProgress variant="determinate" value={data[3].value} />
+              </div>
+              <div className="check">
+                <Checkbox
+                  checked={checked.Four}
+                  onClick={handleChangeFour}
+                  name="four"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              </div>
             </div>
             <div className="result-cat">
-              <span>Cat 5: 0%</span>
-              <BorderLinearProgress variant="determinate" value={1} />
+              <div className="bar">
+                <span>{data[4].category+': '+data[4].value}%</span>
+                <BorderLinearProgress variant="determinate" value={data[4].value} />
+              </div>
+              <div className="check">
+                <Checkbox
+                  checked={checked.Five}
+                  onClick={handleChangeFive}
+                  name="five"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              </div>
             </div>
           </div>
-          <Button
-            variant="contained" 
-            className="btn-upload"
-          >
+          <div className="btn-group">
+            <Button
+              variant="contained" 
+              className="btn-upload"
+            >
               Guardar imagen
-          </Button>
+            </Button>
+            <Button
+              variant="contained" 
+              className="btn-upload"
+              onClick={handleEstimate}
+            >
+              Estimar calorias
+            </Button>
+          </div>
         </div>
 
       </div>
